d08e7eacc106b7431b885716b0cbcf388e0c2b49
Mathieu Marquis <mathieu.marquis.2@ulaval.ca>
2024-07-29 01:07:24 -0400

    Communication functional !

---
+++ stupid_test.py
@@ -0,0 +1,24 @@
+import time
+
+
+current_num = 0
+while True:
+    time.sleep(0.01)
+    try:
+        with open(r"C:\Users\Proprio\Documents\Mathieu\bisbille_des_galaxies\test_measurements.txt", "r") as f:
+            lines = f.readlines()
+            if len(lines) > current_num:
+                infos = lines[-1].split("\t")
+                if float(infos[11]) > 50:   # peak saturation
+                    print(f"x: {infos[-4]} y: {infos[-3]}")
+                else:
+                    print("Off")
+                current_num = len(lines)
+    except PermissionError:
+        print("PermissionError encountered")
+    
+    if current_num % 100 == 0:
+        with open(r"C:\Users\Proprio\Documents\Mathieu\bisbille_des_galaxies\test_measurements.txt", "w") as f:
+            f.write("NOTHING")
+            print("wrote")
+            current_num = 1
