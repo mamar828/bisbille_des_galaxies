8d8326285070ae27cfbff625f88084ca4a1ad9e8
Mathieu Marquis <mathieu.marquis.2@ulaval.ca>
2024-07-28 22:48:41 -0400

    stdafx.h

---
+++ test.cpp
@@ -0,0 +1,16 @@
+//#include <CFile.h>
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+CFile m_File;
+
+int bResult = m_File.Open(_T("\\\\.\\pipe\\pipe_beamage"), CFile::modeReadWrite);
+
+if (bResult) AfxMessageBox(_T("File opened"));
+else		 AfxMessageBox(_T("File NOT opened"));
+
+m_bOpenPipe = 1;
+
+cout << "Hello world" << endl;
